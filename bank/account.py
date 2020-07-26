31fb4550d576b050b092d3e306065bec006b7367
Hellenivy <hellenakinyiivy@gmail.com>
2020-07-26 11:39:41 +0300

    git commit changes

--- bank/account.py
+++ bank/account.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 
     def __init__(self, first_name, last_name,phone_no,bank): 
         self.self.first_name = first_name
@@ -14,64 +15,98 @@
             self.bank, self.first_name, self.last_name, self.phone_no, self.bank)
         return name
      
-   
     def deposit(self, amount):
+          try: 
+          amount + 1
+          except:TypeError
+          print("Please enter amount in figures")
+          return
+
         if amount <=0:
         print("You can\'t deposit zero or negative")
-
+        
         else:
-        self.balance += amount
-        print("You have deposited {} to {}".format(amount, self.account_name()))
+         self.balance += amount:
+         self.deposits.append(amount)
+            formated_time = time.strftime("%A, %drd %B %Y, %H:%M %p")
+        print("You have deposited {} to {}".format(amount, self.account_name(),formated_time))
         return
 
     def withdraw(self, amount):
-         
+        try:
+            amoount + 1
+        except TypeError:
+            print("You must enter the amount in figures")
+            return
+ 
         if amount <= 0:
           print("You can\'t withdraw zero or negative")
-        
 
         elif amount > self.balance:
             print("Insufficient funds")
         else:
             self.balance -= amount
+            self.withdrawals.append(amount)
             print("You have withdrawn {} from {}".format(amount, self.account_name()))
-       
-                    
-    def _get_balance(self):
-        return "The balance for {} is {}".format(self.account_name(), self.balance)
-
-
-    def request_loan(self,amount):
-      if amount <= 0:
-        print("Invalid request")
-        
-      else: 
-        self.amount = 0
-        print("You have borrowed a loan of {}".format(amount)
       
+    def get_balance(self):
+     return "The balance for {} is {}".format(self.account_name(), self.balance)
+  
+    def show_deposit_statements(self):
+        for deposit in self.deposits:
+            formated_time = time.strftime("%A, %drd %B %Y, %H:%M %p")
+            print("{} deposited on {}".format(deposit, formated_time))
+ 
+    def show_withdrawals_statement(self):
+        for withdraw in  self.withdrawals:
+            print(withdraw)
+
+    def request_loan(self, amount):
+        try:
+            amount + 1
+        except TypeError:
+            print("Please enter the amount in figures")
+            return
+
+        if amount <= 0:
+            print("You can\'t request for an amount low than zero")
+        else:
+             self.loan = amount
+             print("You have been given a loan of {}".format(amount))
+
     def repay_loan(self, amount):
-      if loan <= 0:
-        print("Invalid amount to reduce your loan")
-      elif self.loan == 0
-        print(" You have repaid {}".format(loan))
-      elif amount > self.loan
-        print("Your loan balance is {} please enter an amount that is less or equal".format(self.loan))
-      else:
-        self.loan-=amount
-        print("You have paid your loan with {} .Your loan balance is {}"format,self.loan)
+        try:
+            amount + 1
+        except TypeError:
+            print("Please enter the amount in figures")
+            return
 
-    
+        if amount <= 0:
+            print("Too low to repay your amount")
+        elif self.loan == 0:
+            print("You don't have a loan at the moment")
+        elif amount > self.loan:
+            print("Your loan is {} please enter an amount that is less or equal".format(self.loan))
+        else:
+            self.loan -= amount
+            print("You have repaid you loan with {} your balance is {}".format(amount, self.loan))
+
 acc1 = BankAccount("Noisemaker" ,"Owili",+2567093886720,"Tropical")
-print(acc1.phone_no)
+acc1.first_name
 acc1.deposit(100000)
-acc1.withdraw(5000)
-acc1.withdraw(75)
+print(acc1.first_name)
 acc1.deposit(200100)
-acc1.deposit(900)
-acc1.lend_loan(80000)
-acc1.lend_loan(45000)
-acc1.pay_loan(90800)
-print(acc1.getloan_balance())
-print(acc1.deposit_summary())
-
-
+print(acc1.balance)
+acc1.request_loan(8000)
+acc1.repay_loan(3000)
+acc1.repay_loan(45688)
+acc1.repay_loan(4000)
+print(acc1.loan)
+print(acc1.balance)
+acc1.request_loan(7840)
+acc1.deposit(769)
+acc1.deposit("eighty")
+acc1.deposit(3456)
+acc1.deposit(1234)
+acc1.show_deposit_statements()
+acc1.request_loan("ninety")
